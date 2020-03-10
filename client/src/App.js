d565ece91d6c30fe196d23b9c1123e62d36e553a
fzea01 <nuttahpon_best@hotmail.com>
2020-03-10 21:29:07 +0700

    modifine app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Student from './components/Student'
+import StudentList from './components/student/StudentList'
+import InputForm from './components/student/InputForm';
+import Nevbar from './components/nevbar/menu'
+import './App.css'
 
 const App = () => {
   return (
-    <div >
-      <Student />
+    <div className='body-App'>
+      <Nevbar />
+      <h1>Insert Data</h1>
+      <InputForm />
+      <h1>Students Infomation</h1>
+      <StudentList />
     </div>
   );
 }
--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect}  from 'react'
 import axios from 'axios'
 import '../css/style.css'
-let port = 8080
+const port = 8080
 
 
 const Student = () => {
